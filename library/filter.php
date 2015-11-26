40cc625849247a5d7c463ae593f3378cb105ffa9
julian-weiland <julianweiland@icloud.com>
2015-11-26 22:47:47 +0100

    Filter update

--- library/filter.php
+++ library/filter.php
@@ -47,33 +47,12 @@ function gearlist_filter($posts = '') {
 	  </div>
 	  <div class="content" id="panel2">
 	  	<span class="label filteritem" data-filter="singlegearitem">Alle</span>
-		<?php 
-		/*$brands = get_terms( 'brand', 'orderby=name&hide_empty=0' );
-		$tabs = "<ul class='tabs' data-tab><li class='tab-title active'><a href='#brandA'><Hersteller A</li>";
-		$tabs_content = '<div class="content active" id="brandA">'
-		$groupFirstLetter = 'A';
-		foreach ($brands as $brand) {
-			$firstletter = $brand->name;
-			$firstletter = $firstletter[0];
-			if ($firstletter != $groupFirstLetter) {
-				$tabs .= "<li class='tab-title'>Hersteller " . $firstletter . "</li>";
-				//echo '<optgroup label="' . $firstletter . '">';
-				$tabs_content .= '</div><div class="content" id="panel'. $firstletter . '">';
-				$groupFirstLetter = $firstletter;
-			}
-			$tabs_content .= '<span class="label filteritem ' . $brand->slug . '" data-filter="brand-' . $brand->slug . '">' . $brand->name . '</span> ';
-		};
 
-		$tabs .= '</ul>';
-		$tabs_content .= '</div>'
-		echo $tabs;
-		echo $tabs_content;*/
-
-		?>
 
 		<?php 
 
 	$usedBrands = array();
+	$usedBrandsSlug = array();
 	global $post;
 	foreach( $posts as $post ): setup_postdata( $post );
 		$brands = wp_get_post_terms( get_the_ID(), 'brand');
@@ -82,41 +61,43 @@ function gearlist_filter($posts = '') {
 		//echo "<hr />";
 		if (isset($brands[0])) {
 			$brandslug = $brands[0]->slug;
-			if (in_array($brandslug, $usedBrands)) {
+			if (in_array($brandslug, $usedBrandsSlug)) {
 				# code...
 			}else{
 				//echo $brands[0]->name . "<br />";
-				echo '<span class="label filteritem" data-filter="brand-' . $brandslug . '">' . $brands[0]->name . '</span> ';
-				$usedBrands[] = $brands[0]->slug;
+				//echo '<span class="label filteritem" data-filter="brand-' . $brandslug . '">' . $brands[0]->name . '</span> ';
+				$brandinfo = array('slug' => $brands[0]->slug, 'name' => $brands[0]->slug );
+				//$usedBrands[] = $brands[0]->slug;
+				$usedBrands[] = $brandinfo;
+				$usedBrandsSlug[] = $brands[0]->slug;
 			};
-		}
+		};
 		
 
 	endforeach;
+/**
+Array Sortieren
+*/	
+foreach ($usedBrands as $nr => $inhalt)
+{
+    $slug[$nr]  = strtolower( $inhalt['slug'] );
+    $name[$nr]   = strtolower( $inhalt['name'] );
+    //$datum[$nr] = strtolower( $inhalt['Datum'] );
+}
+array_multisort($name, SORT_ASC, $usedBrands);
+
+
+foreach ($usedBrands as $key => $value) {
+	echo $value['name'];
+	echo "-";
+	echo $value['slug'];
+	echo " | ";
+}
+//print_r($usedBrands);
 
 
 		 ?>
 
-<!--<ul class="tabs" data-tab>
-  <li class="tab-title active"><a href="#panel1">Tab 1</a></li>
-  <li class="tab-title"><a href="#panel2">Tab 2</a></li>
-  <li class="tab-title"><a href="#panel3">Tab 3</a></li>
-  <li class="tab-title"><a href="#panel4">Tab 4</a></li>
-</ul>
-<div class="tabs-content">
-  <div class="content active" id="panel1">
-    <p>This is the first panel of the basic tab example. You can place all sorts of content here including a grid.</p>
-  </div>
-  <div class="content" id="panel2">
-    <p>This is the second panel of the basic tab example. This is the second panel of the basic tab example.</p>
-  </div>
-  <div class="content" id="panel3">
-    <p>This is the third panel of the basic tab example. This is the third panel of the basic tab example.</p>
-  </div>
-  <div class="content" id="panel4">
-    <p>This is the fourth panel of the basic tab example. This is the fourth panel of the basic tab example.</p>
-  </div>
-</div>-->
 
 
 	  </div>
